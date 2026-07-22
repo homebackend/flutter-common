c491575b3eb7671287f6218e46f74848dd514f9c
Neeraj <neerajcd@gmail.com>
2026-07-22 08:32:01 +0530

    Moving page common functionality to here

---
+++ lib/mixin/page_common.dart
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 Neeraj Jakhar
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_common/mixin/main_config_manager.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+mixin PageCommon {
+  bool get mounted;
+  BuildContext get context;
+  FlutterSecureStorage get secureStorage;
+
+  void showSnackBar(String message) {
+    if (mounted) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(message)));
+    }
+  }
+
+  List<Widget> getAppBarCommonActions(MainConfigManager configManager) => [
+    IconButton(
+      icon: const Icon(Icons.output),
+      tooltip: 'Export Settings',
+      onPressed: () async {
+        final msg = await configManager.exportSystemPreferences();
+        if (msg != null) showSnackBar(msg);
+      },
+    ),
+  ];
+}
